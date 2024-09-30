c448185a99b4f3c477e92d0bd30d7d1fde744cda
Alessandro Stolfo <alessandro.stolfo@gmail.com>
2024-09-30 23:40:37 +0200

    kw val

---
+++ data_editing.py
@@ -0,0 +1,38 @@
+# %%
+import pandas as pd
+import json
+import os
+import sys
+import random
+sys.path.append('/Users/alestolfo/workspace/llm-steer-instruct/')
+# %%
+path = './data/keyword_test.jsonl'
+
+with open(path, 'r') as f:
+    results = [json.loads(line) for line in f]
+
+data_df = pd.DataFrame(results)
+# %%
+phrasings_include = [' Include the word {} in your answer.', ' Make sure to include the word "{}".', ' The output should contain the word "{}".', ' The output must contain the word "{}".', ' Make sure that the word "{}" is included in the output.', ' The output must include the word "{}".']
+phrasings_exclude = [' Do not include the word {}.', ' Make sure not to include the word "{}".', ' Do not use the word {}.', ' Do not say "{}".', ' Please exclude the word "{}".', ' The output should not contain the word "{}".']
+new_rows = []
+for i, r in data_df.iterrows():
+    for word in r['likely_words']:
+        new_row = r.copy()
+        new_row['prompt_without_instruction'] = r['question'] 
+        # new_row["instruction_id_list"] = ["keywords:existence"]
+        new_row["instruction_id_list"] = ["keywords:forbidden_words"]
+        # new_row["instruction_id_list_for_eval"] = ["keywords:existence"]
+        new_row["instruction_id_list_for_eval"] = ["keywords:forbidden_words"]
+        # new_row['kwargs'] = [{"keywords": [word]}]
+        new_row['kwargs'] = [{"forbidden_words": [word]}]
+        phrasing = random.choice(phrasings_exclude)
+        new_prompt = new_row['prompt_without_instruction'] + phrasing.format(word)
+        new_row['prompt'] = new_prompt
+        new_rows.append(new_row)
+
+new_data_df = pd.DataFrame(new_rows)
+# %%
+# store the new data
+new_data_df.to_json('./data/keyword_test_exclusion_likely.jsonl', orient='records', lines=True)
+# %%
