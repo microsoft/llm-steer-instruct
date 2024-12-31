287a98eb51941239788167ea5427dd03a34a217c
Alessandro Stolfo <alessandro.stolfo@gmail.com>
2024-12-31 11:27:44 +0100

    ls 2b

--- submit_layer_search_format.sh
+++ submit_layer_search_format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 model_name="phi-3"
-# model_name="gemma-2-2b-it"
+model_name="gemma-2-9b"
 
 gpu_req="a100_80gb:1"
 # gpu_req="rtx_4090:1"
@@ -10,18 +10,18 @@ gpu_req="rtx_3090:1"
 # arguments
 dry_run=false
 
-constraint=exclude_w_exclude_rep
-max_generation_length=256
-n_examples_per_instruction=10
+max_generation_length=128
+n_examples_per_instruction=6
+include_instruction=false
 
 
-sbatch --output="${HOME}/bsub_logs/steering/format/search-${model_name}-ex${n_examples_per_instruction}" \
+sbatch --output="${HOME}/bsub_logs/steering/format/search-${model_name}-${include_instruction}-ex${n_examples_per_instruction}" \
     --job-name="ls-${model_name}" \
     -n 4 \
     --gpus=1 \
-    --gres=gpumem:30g \
+    --gres=gpumem:50g \
     --mem-per-cpu=50G \
-    --time=23:59:00 \
---wrap="python ifeval_experiments/find_best_layer.py model_name=$model_name max_generation_length=$max_generation_length n_examples_per_instruction=$n_examples_per_instruction
+    --time=47:59:00 \
+--wrap="python ifeval_experiments/find_best_layer.py model_name=$model_name max_generation_length=$max_generation_length n_examples_per_instruction=$n_examples_per_instruction include_instruction=$include_instruction dry_run=$dry_run
 "
 
