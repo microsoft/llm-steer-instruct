1bcca685fcfe70651e1892536b82949f670c31ad
Alessandro Stolfo <alessandro.stolfo@gmail.com>
2025-01-01 05:50:20 +0100

    g no it

--- submit_layer_search_format.sh
+++ submit_layer_search_format.sh
@@ -13,6 +13,7 @@ dry_run=false
 max_generation_length=128
 n_examples_per_instruction=6
 include_instruction=false
+cross_model_steering=true
 
 
 sbatch --output="${HOME}/bsub_logs/steering/format/search-${model_name}-${include_instruction}-ex${n_examples_per_instruction}" \
@@ -21,7 +22,7 @@ sbatch --output="${HOME}/bsub_logs/steering/format/search-${model_name}-${includ
     --gpus=1 \
     --gres=gpumem:50g \
     --mem-per-cpu=50G \
-    --time=47:59:00 \
---wrap="python ifeval_experiments/find_best_layer.py model_name=$model_name max_generation_length=$max_generation_length n_examples_per_instruction=$n_examples_per_instruction include_instruction=$include_instruction dry_run=$dry_run
+    --time=23:59:00 \
+--wrap="python ifeval_experiments/find_best_layer.py model_name=$model_name max_generation_length=$max_generation_length n_examples_per_instruction=$n_examples_per_instruction include_instruction=$include_instruction dry_run=$dry_run cross_model_steering=$cross_model_steering
 "
 
