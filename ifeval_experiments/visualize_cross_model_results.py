7620f84d88376423c202d3371138df430ba67351
Alessandro Stolfo <alessandro.stolfo@gmail.com>
2025-01-03 09:56:35 +0100

    eval cm

--- ifeval_experiments/visualize_cross_model_results.py
+++ ifeval_experiments/visualize_cross_model_results.py
@@ -20,7 +20,7 @@ from eval.evaluation_main import test_instruction_following_loose
 import plotly
 # %%
 overall_accuracies = {}
-model_names = ['gemma-2-2b',] #'gemma-2-9b']
+model_names = ['gemma-2-2b', 'gemma-2-9b']
 for model_name in model_names:
     single_instr = 'single_instr/all_base_x_all_instr'
     mode = 'no_instr'
@@ -42,7 +42,7 @@ for model_name in model_names:
         results = [json.loads(r) for r in results]
     results_df_steering = pd.DataFrame(results)
 
-    mode = 'adjust_rs_-1_perplexity_cross_model'
+    mode = 'adjust_rs_-1_perplexity_cross_model_old'
     path_to_results = f'./ifeval_experiments/out/{model_name}/{single_instr}/{mode}/{subset}eval_results_{eval_type}.jsonl'
     with open(path_to_results) as f:
         results = f.readlines()
@@ -63,7 +63,7 @@ for model_name in model_names:
         results = [json.loads(r) for r in results]
     results_df_instr_plus_steering = pd.DataFrame(results)
 
-    mode = 'instr_plus_adjust_rs_-1_perplexity_cross_model_old'
+    mode = 'instr_plus_adjust_rs_-1_perplexity_cross_model'
     path_to_results = f'./ifeval_experiments/out/{model_name}/{single_instr}/{mode}/{subset}eval_results_{eval_type}.jsonl'
     with open(path_to_results) as f:
         results = f.readlines()
@@ -166,7 +166,7 @@ for model_name in model_names:
 
 # %%
 setting = '<b>w/o</b> Instr.'
-setting = '<b>w/</b> Instr.'
+# setting = '<b>w/</b> Instr.'
 
 # Create a DataFrame for overall accuracy
 df_overall_2b = pd.DataFrame(overall_accuracies['gemma-2-2b'])
